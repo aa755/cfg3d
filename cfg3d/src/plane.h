78d959c32cd8c096e5931c95ec7e8f2f8300947d
Paul Yang <heranyang@hotmail.com>
2011-11-02 00:47:40 -0400

    done

---
+++ cfg3d/src/plane.h
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <pcl/point_types.h>
+#include "pcl/ModelCoefficients.h"
+
+struct plane{
+  std::vector<pcl::PointXYZRGB> pts;
+  pcl::ModelCoefficients normal;
+  int points;
+};
+
+struct orientation{
+  float x;
+  float y;
+  float z;
+};
