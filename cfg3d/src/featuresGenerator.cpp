5b1eb57aee249cd1fdd7012c0a409186ea9aa284
Sherwin Li <lisherwin@ubuntu.(none)>
2011-11-27 10:31:36 -0800

    appendrules and nonintermediate terminal fix

--- cfg3d/src/featuresGenerator.cpp
+++ cfg3d/src/featuresGenerator.cpp
@@ -17,48 +17,82 @@ void createDataStructuresImports() {
     outputDataStructuresCode<<importString.append(import)<<endl;
 }
 
-void createLearnerImports() {
+void createLearnerImports(string objectName) {
     string importString = "#include ";
 
     string import = "<queue>";
     outputLearnerCode<<importString.append(import)<<endl;
     
     importString = "#include ";
-    import = "\"generatedDataStructures.cpp\"";
-    outputLearnerCode<<importString.append(import)<<endl;
+    import = "generatedDataStructures.cpp";
+    outputLearnerCode<<importString<<"\""<<objectName<<"_"<<import<<"\""<<endl;
 }
 
 // Data Structures
-void createClassAndWrite(string dataStructure) {
+//void createClassAndWrite(string dataStructure) {
+//    string front = "class ";
+//    string className = dataStructure;
+//    if (className != "Plane") {
+//        string back = " : public NonTerminalIntermediate{};";
+//        outputDataStructuresCode<<front.append(className).append(back)<<endl;
+//    }
+//}
+
+void createNonIntermediateClassAndWrite(string dataStructure) {
     string front = "class ";
     string className = dataStructure;
-    if (className != "Plane") {
-        string back = " : public NonTerminalIntermediate{};";
-        outputDataStructuresCode<<front.append(className).append(back)<<endl;
-    }
+    string back = " : public NonTerminal{};";
+    outputDataStructuresCode<<front.append(className).append(back)<<endl;
 }
 
-void writeDataStructuresToFile(set<string> dataStructuresSet) {
+void createIntermediateClassAndWrite(string dataStructure) {
+    string front = "class ";
+    string className = dataStructure;
+    string back = " : public NonTerminalIntermediate{};";
+    outputDataStructuresCode<<front.append(className).append(back)<<endl;
+}
+
+//void writeDataStructuresToFile(set<string> dataStructuresSet) {
+//    BOOST_FOREACH(string dataStructure, dataStructuresSet) {
+//        createIntermediateClassAndWrite(dataStructure);
+//    }
+//}
+
+void writeIntermediateDataStructuresToFile(set<string> dataStructuresSet) {
     BOOST_FOREACH(string dataStructure, dataStructuresSet) {
-        createClassAndWrite(dataStructure);
+        createIntermediateClassAndWrite(dataStructure);
     }
 }
 
-void createDataStructureFromString(string line, set<string>& dataStructuresSet) {
+void writeNonIntermediateDataStructuresToFile(set<string> dataStructuresSet) {
+    BOOST_FOREACH(string dataStructure, dataStructuresSet) {
+        createNonIntermediateClassAndWrite(dataStructure);
+    }
+}
+
+void createDataStructureFromString(string line, set<string>& intermediateDataStructuresSet, set<string>& nonIntermediateDataStructuresSet, string objectName) {
     char_separator<char> sep(", ");
     tokenizer<char_separator<char> > tokens(line, sep);
+    vector<string> tokenList;
     BOOST_FOREACH(string t, tokens)
-    {
-        dataStructuresSet.insert(t);
+    {   
+        tokenList.push_back(t);
+    }
+    
+    if (tokenList.size() == 2 || tokenList.at(0) == objectName) {        
+        nonIntermediateDataStructuresSet.insert(tokenList.at(0));
+    } else if (tokenList.size() == 3) {
+        intermediateDataStructuresSet.insert(tokenList.at(0));
     }
 }
 
-void createDataStructures(const char* file) {
+void createDataStructures(const char* file, string objectName) {
     ifstream rulesFile;
     rulesFile.open(file);
 
     string line;
-    set<string> dataStructuresSet;
+    set<string> intermediateDataStructuresSet;
+    set<string> nonIntermediateDataStructuresSet;
     if (rulesFile.is_open())
     {
         while (rulesFile.good()) {
@@ -67,11 +101,14 @@ void createDataStructures(const char* file) {
             {
                 break;
             }
-            createDataStructureFromString(line, dataStructuresSet);
+            createDataStructureFromString(line, intermediateDataStructuresSet, nonIntermediateDataStructuresSet, objectName);
         }
     }
+    writeIntermediateDataStructuresToFile(intermediateDataStructuresSet);
+    writeNonIntermediateDataStructuresToFile(nonIntermediateDataStructuresSet);
     
-    writeDataStructuresToFile(dataStructuresSet);
+    outputDataStructuresCode<<endl;
+    outputDataStructuresCode<<"void "<<objectName<<"AppendLearningRules(vector<RulePtr>& learningRules) {"<<endl;    
 }
 
 
@@ -80,13 +117,13 @@ void createRunLearnFront() {
     outputLearnerCode<<"void runLearn(pcl::PointCloud<PointT> sceneToLearn, char* segmentToLabelFile) {"<<endl;
     outputLearnerCode<<"    initialize(sceneToLearn, segmentToLabelFile);"<<endl;
     outputLearnerCode<<"    queue<Symbol*> nodesCreatedSoFar;"<<endl;
-    outputLearnerCode<<"    vector<Terminal*> temp;"<<endl;
+    outputLearnerCode<<"    vector<Terminal*> temp;"<<endl<<endl;
 }
 
                 // Mid
 void applySingleRule(string ruleName, string terminalLabel) {
     string front = "    nodesCreatedSoFar.push(";
-    outputLearnerCode<<front.append(ruleName).append(".applyRuleLearning(labelToPlanes.at(\"").append(terminalLabel).append("\"), temp));")<<endl;
+    outputLearnerCode<<front.append(ruleName).append(".applyRuleLearning(labelToPlanes.at(\"").append(terminalLabel).append("\"), temp));")<<endl<<endl;
 }
 
 void applyDoubleRule(string ruleName, string LHS, string RHS1, string RHS2) {
@@ -101,35 +138,46 @@ void applyDoubleRule(string ruleName, string LHS, string RHS1, string RHS2) {
     outputLearnerCode<<tab2.append(RHS2).append("* ").append(last).append(" = dynamic_cast<").append(RHS2).append("*>(nodesCreatedSoFar.front());")<<endl;
     outputLearnerCode<<"    nodesCreatedSoFar.pop();"<<endl;
     string front = "    nodesCreatedSoFar.push(";
-    outputLearnerCode<<front.append(ruleName).append(".applyRuleLearning(").append(secondToLast).append(", ").append(last).append(", temp));")<<endl;
+    outputLearnerCode<<front.append(ruleName).append(".applyRuleLearning(").append(secondToLast).append(", ").append(last).append(", temp));")<<endl<<endl;
 }
 
 void createApplyRule(vector<string> rulesVector) {
     int count =  rulesVector.size();
     string LHS = rulesVector.at(0);
     string RHS;
-    string rule;
     string ruleType;
     if (count == 2) {
         RHS = rulesVector.at(1);
-        string ruleType = "    SingleRule";
+        string tab = "    ";
+        string ruleType = "SingleRule";
         string temp = "";
         string ruleName = temp.append("rule").append(LHS);
-        rule = ruleType.append("<").append(LHS).append(", ").append(RHS).append("> ").append(ruleName).append("(true);");
-        outputLearnerCode<<rule<<endl;
+        string learningRule = ruleType.append("<").append(LHS).append(", ").append(RHS).append("> ").append(ruleName).append("(true)");
+        ruleType = "SingleRule";
+        string inferenceRule = ruleType.append("<").append(LHS).append(", ").append(RHS).append(">").append("()");
+        outputLearnerCode<<tab<<learningRule<<";"<<endl;
+        
+        string newString = "    learningRules.push_back(RulePtr(new ";
+        outputDataStructuresCode<<newString.append(inferenceRule).append("));")<<endl;
+        
         applySingleRule(ruleName, LHS);
-        outputLearnerCode<<endl;
     }
     else if (count == 3) {
         string RHS1 = rulesVector.at(1);
         string RHS2 = rulesVector.at(2);
-        ruleType = "    DoubleRule";
+        string tab = "    ";
+        ruleType = "DoubleRule";
         string temp = "";
         string ruleName = temp.append("rule").append(LHS);
-        rule = ruleType.append("<").append(LHS).append(", ").append(RHS1).append(", ").append(RHS2).append("> ").append(ruleName).append("(true);");
-        outputLearnerCode<<rule<<endl;
+        string learningRule = ruleType.append("<").append(LHS).append(", ").append(RHS1).append(", ").append(RHS2).append("> ").append(ruleName).append("(true);");
+        ruleType = "DoubleRule";
+        string inferenceRule = ruleType.append("<").append(LHS).append(", ").append(RHS1).append(", ").append(RHS2).append(">").append("()");
+        outputLearnerCode<<tab<<learningRule<<";"<<endl;
+        
+        string newString = "    learningRules.push_back(RulePtr(new ";
+        outputDataStructuresCode<<newString.append(inferenceRule).append("));")<<endl;
+        
         applyDoubleRule(ruleName, LHS, RHS1, RHS2);
-        outputLearnerCode<<endl;
     }    
 }
 
@@ -200,14 +248,15 @@ int main(int argc, char** argv) {
     outputDataStructuresCode.open(dataStructuresFileFront.c_str());
     const char* rulesFile = fileFront.c_str();
     createDataStructuresImports();
-    createDataStructures(rulesFile);
+    createDataStructures(rulesFile, argv[3]);
     
     ///////////////////// Generating Learner ////////////////////
     string learnerFileFront = "";
     learnerFileFront.append(argv[3]).append("_generatedLearner.cpp");
     outputLearnerCode.open(learnerFileFront.c_str());
-    createLearnerImports();
+    createLearnerImports(argv[3]);
     createLearner(rulesFile);
+    outputDataStructuresCode<<"}"<<endl;
     
     return 0;
 }
