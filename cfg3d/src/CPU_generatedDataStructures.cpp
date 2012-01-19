5a7520f14a1f69b22a96dd9f249a15dcb1cdfd42
Sherwin Li <lisherwin@ubuntu.(none)>
2012-01-18 20:37:58 -0800

    commiting generated cpps

--- cfg3d/src/CPU_generatedDataStructures.cpp
+++ cfg3d/src/CPU_generatedDataStructures.cpp
@@ -1,11 +1,8 @@
 #include "helper.cpp"
-class CPUTop_CPURSide : public NonTerminalIntermediate{};
-//class CPUTop_CPURSide : public Scene{};
-class CPUTop_CPURSide_CPUFront : public NonTerminalIntermediate{};
-//class CPUTop_CPURSide_CPUFront : public Scene{};
-//class CPUTop_CPURSide_CPUFront_CPUBack : public Scene{};
-class CPUTop_CPURSide_CPUFront_CPUBack : public NonTerminalIntermediate{};
-class CPU : public Scene{};
+class CPU : public NonTerminal{};
+class CPUFront_CPUTop : public NonTerminalIntermediate{};
+class CPUFront_CPUTop_CPURSide : public NonTerminalIntermediate{};
+class CPUFront_CPUTop_CPURSide_CPUBack : public NonTerminalIntermediate{};
 class CPUBack : public PlanarPrimitive{};
 class CPUFront : public PlanarPrimitive{};
 class CPULSide : public PlanarPrimitive{};
@@ -13,13 +10,13 @@ class CPURSide : public PlanarPrimitive{};
 class CPUTop : public PlanarPrimitive{};
 
 void CPUAppendLearningRules(vector<RulePtr>& learningRules) {
-    learningRules.push_back(RulePtr(new SingleRule<CPUTop, Plane>()));
-    learningRules.push_back(RulePtr(new SingleRule<CPURSide, Plane>()));
-    learningRules.push_back(RulePtr(new DoubleRule<CPUTop_CPURSide, CPUTop, CPURSide>()));
     learningRules.push_back(RulePtr(new SingleRule<CPUFront, Plane>()));
-    learningRules.push_back(RulePtr(new DoubleRule<CPUTop_CPURSide_CPUFront, CPUTop_CPURSide, CPUFront>()));
+    learningRules.push_back(RulePtr(new SingleRule<CPUTop, Plane>()));
+    learningRules.push_back(RulePtr(new DoubleRule<CPUFront_CPUTop, CPUFront, CPUTop>()));
+    learningRules.push_back(RulePtr(new SingleRule<CPURSide, Plane>()));
+    learningRules.push_back(RulePtr(new DoubleRule<CPUFront_CPUTop_CPURSide, CPUFront_CPUTop, CPURSide>()));
     learningRules.push_back(RulePtr(new SingleRule<CPUBack, Plane>()));
-    learningRules.push_back(RulePtr(new DoubleRule<CPUTop_CPURSide_CPUFront_CPUBack, CPUTop_CPURSide_CPUFront, CPUBack>()));
+    learningRules.push_back(RulePtr(new DoubleRule<CPUFront_CPUTop_CPURSide_CPUBack, CPUFront_CPUTop_CPURSide, CPUBack>()));
     learningRules.push_back(RulePtr(new SingleRule<CPULSide, Plane>()));
-    learningRules.push_back(RulePtr(new DoubleRule<CPU, CPUTop_CPURSide_CPUFront_CPUBack, CPULSide>()));
+    learningRules.push_back(RulePtr(new DoubleRule<CPU, CPUFront_CPUTop_CPURSide_CPUBack, CPULSide>()));
 }
