3fd8fac12b9886555209f8f7cbebe2e7c7e60cf7
Sherwin Li <lisherwin@ubuntu.(none)>
2012-01-21 17:30:57 -0800

    adding labelToLabelMapping feature

--- cfg3d/src/invert.py
+++ cfg3d/src/invert.py
@@ -18,23 +18,42 @@ def getFileWrite(name):
     return open(name, 'w')
 
 def printUsage():
-    print '\nUsage: fileToBeInverted'
+    print '\nUsage: fileToBeInverted labelToLabelIndexMapping'
 
-def invert(fileName):
+def createMappingDict(mappingFileName):
+    mapping = {}
+    file = getFile(mappingFileName)
+    for line in file:
+        vector = line.rstrip('\n').split(',')
+        key = vector[0]
+        value = vector[1]
+        if mapping.has_key(key):
+            sys.exit("Duplicate labeling")
+        else:
+            mapping[key] = value
+    return mapping
+
+def invert(fileName, mappingFileName):
     mapping = {}
     file = getFile(fileName)
     writeFile = getFileWrite(''.join([fileName,'_inverted']))
+
+    mappingDict = createMappingDict(mappingFileName)
+
     for line in file:
         vector = line.rstrip('\n').split(',')
         head = vector[0]
         if not len(vector) == 1:
             tail = vector[1:]
             for elem in tail:
-                writeFile.write(''.join([head,',',elem,'\n']))
+                if mappingDict.has_key(head):
+                    writeFile.write(''.join([elem,',',mappingDict[head],'\n']))
+                else:
+                    sys.exit(''.join(['No corresponding labelIndex in labelToLabelIndexFile: ',head]))
 
 def main():
-    if len(sys.argv) == 2:
-        invert(sys.argv[1])
+    if len(sys.argv) == 3:
+        invert(sys.argv[1], sys.argv[2])
     else:
         printUsage()
     pass
