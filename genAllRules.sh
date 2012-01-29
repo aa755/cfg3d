882ae9760298972f4f0e481679cca4f946658c38
Abhishek Anand <aa755@cs.cornell.edu>
2012-01-28 19:19:16 -0500

    added rule script

---
+++ genAllRules.sh
@@ -0,0 +1,26 @@
+rulefolder=~/cfgProject/cfg3d/rules
+cd $rulefolder
+rm  *
+cd /home/aa755/cfgProject/data/gold/realData/CPU/gold/featureLearning
+./genCPUFeats.sh
+#/home/aa755/cfgProject/data/gold/realData/CPU/gold/featureLearning/genCPUFeats.sh
+cp rule_* $rulefolder/
+
+
+cd /home/aa755/cfgProject/data/gold/realData/monitorTripod/final
+./genMonitorFeats.sh
+#/home/aa755/cfgProject/data/gold/realData/monitorTripod/final/genMonitorFeats.sh
+cp rule_* $rulefolder/
+
+
+
+cd /home/aa755/cfgProject/data/localBackup/allPCDs/wallPCDs
+./learn.sh
+#/home/aa755/cfgProject/data/localBackup/allPCDs/wallPCDs/learn.sh
+cp rule_* $rulefolder/
+
+
+# manual rules
+cd $rulefolder
+echo "0.9305,0.2428,0.6000,1.00" > rule_5Floor__5Plane.out
+echo "0.0021,0.0663,-0.100,0.1" >> rule_5Floor__5Plane.out
