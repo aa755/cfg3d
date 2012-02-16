afd1b11be77b321f42946f2029218efd32543948
Abhishek Anand <aa755@cs.cornell.edu>
2012-02-16 17:20:06 -0500

    parsed the ordering

--- cfg3d/src/featGenGen.cpp
+++ cfg3d/src/featGenGen.cpp
@@ -24,11 +24,12 @@ class TreeNode
     
 public:
     typedef pair<string,set<string> > RuleType;
-//    static map< RuleType , vector<string> > ruleOrdering;
-    static map< RuleType , int > ruleOrdering;
-    
     typedef  shared_ptr<TreeNode> Ptr;
     typedef  weak_ptr<TreeNode> WPtr;
+    
+    static map< RuleType , vector<string> > ruleOrdering;
+    static map<string,Ptr> nameToTreeNode;
+
     
     vector<Ptr> children;
     WPtr parent;
@@ -106,7 +107,6 @@ public:
         std::ifstream fileI;
         std::string line;
 
-        map<string,Ptr> nameToTreeNode;
         fileI.open(treeFile);
         getline(fileI, line);
         assert(line.substr(0, 7) == "digraph");
@@ -169,31 +169,70 @@ public:
         return rootNode;
 
     }
+    
+    static void parseMapping(char * file)
+    {
+        std::ifstream fileI;
+        std::string line;
+
+        fileI.open(file);
+
+        
+        if (fileI.is_open())
+        {
+            while (fileI.good())
+            {
+                getline(fileI, line);
+                
+                if (line.size() == 0)
+                    break;
+                
+                RuleType rule;
+                vector<string> toks;
+                getTokens(line, toks, ";");
+                assert( toks.size()==2);
+                string ruleStr=toks.at(0);
+                string order =toks.at(1);
+                getTokens(ruleStr, toks, " ,");
+                rule.first = toks.at(0);
+                for(int i=1;i<(int)toks.size();i++)
+                {
+                    rule.second.insert(toks.at(i));
+                }
+
+                getTokens(order, toks, " ,");
+                
+                // check that all members of the ordering are actually
+                // present at the end of rule
+                
+                assert(rule.second.size()==toks.size());
+                for(int i=0;i<(int)toks.size();i++)
+                {
+                    assert(rule.second.find(toks.at(i))!=rule.second.end());
+                }
+                
+                ruleOrdering[rule]=toks;
+                //cout<<rule<<endl;
+            }
+        }
+    }
 };
 
-map< TreeNode::RuleType , int > TreeNode::ruleOrdering;
+map< TreeNode::RuleType , vector<string> > TreeNode::ruleOrdering;
+map<string,TreeNode::Ptr> TreeNode::nameToTreeNode;
+
 /*
  * 
  */
 int main(int argc, char** argv)
 {
-    TreeNode::RuleType rul1,rul2;
-    rul1.first="Chair";
-    rul2.first="Chair";
-    rul1.second.insert("Base");
-    rul1.second.insert("Back");
-
-    rul2.second.insert("Back");
-//    rul2.second.insert("Base");
-    rul2.second.insert("rest");
-    
-    TreeNode::ruleOrdering[rul1]=1;
-    cout<<TreeNode::ruleOrdering[rul2]<<endl;
 
     
-   // TreeNode::Ptr root=TreeNode::readDotTree(argv[1]);    
-   // root->setFlags();
-   // root->print();
+ //   TreeNode::Ptr root=TreeNode::readDotTree(argv[1]);    
+//    root->setFlags();
+//    root->print();
+    
+    TreeNode::parseMapping(argv[2]);
     return 0;
 }
 
